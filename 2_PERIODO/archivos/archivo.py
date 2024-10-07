1ab81ef463a3c106756dd9bd436de61c5ffd29c4
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-07 09:37:15 -0600

    Adding file1.txt

--- 2_PERIODO/archivos/archivo.py
+++ 2_PERIODO/archivos/archivo.py
@@ -2,3 +2,4 @@ import pathlib
 
 # Crear objeto Path a partir de una cadena
 path1 = pathlib.Path('/Users/alvarogonzalez/Documents/ITC/1_Primer_Semestre/1ER_PERIODO/TC1029-Computational-Thinking/2_PERIODO/archivos/file1.txt')
+print('El objeto Path: ')
