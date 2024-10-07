12782084acc89cc22de44fe96eb3aa3539189c61
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-07 09:49:13 -0600

    Adding Path.home and Path.cwd

--- 2_PERIODO/archivos/archivo.py
+++ 2_PERIODO/archivos/archivo.py
@@ -3,3 +3,13 @@ import pathlib
 # Crear objeto Path a partir de una cadena
 path1 = pathlib.Path('/Users/alvarogonzalez/Documents/ITC/1_Primer_Semestre/1ER_PERIODO/TC1029-Computational-Thinking/2_PERIODO/archivos/file1.txt')
 print('El objeto Path: ')
+print(path1)
+
+# Usando los metodos Path.home() y Path.cwd()
+home = pathlib.Path.home()
+print('El directorio home: ')
+print(home)
+
+current = pathlib.Path.cwd()
+print('El cwd es:')
+print(current)
