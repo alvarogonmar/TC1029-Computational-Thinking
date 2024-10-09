2dbc1024ea7850471ce2ac70fbf7e20ffe85df9c
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 17:11:58 -0600

    act3_1list.py finished

--- 2_PERIODO/practice_for_exam/act3_1list.py
+++ 2_PERIODO/practice_for_exam/act3_1list.py
@@ -11,3 +11,13 @@
 # Debe ser mayor a 0 de lo contrario se solicitará de nuevo hasta que se cumpla la condición.
 # Posteriormente se reciben los n elementos de la lista, uno en cada renglón
 
+elements = int(input('Elements in the list: '))
+
+while elements <= 0:
+    elements = int(input('Elements in the list: '))
+list1 = []
+for i in range(elements):
+    numbers = input(f'Introduce the number {i+1}: ')
+    list1.append(numbers)
+for i in range(len(list1)):
+    print(f'List [{i}] = {list1[i]}')
