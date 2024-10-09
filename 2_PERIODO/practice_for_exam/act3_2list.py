3b6f10d57e320488c42e0d497e86e35d8191b93a
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 17:15:43 -0600

    Adding act3_2list.py

---
+++ 2_PERIODO/practice_for_exam/act3_2list.py
@@ -0,0 +1,14 @@
+#2
+# Lee lista y muestra invertida
+# Escribe un programa que pregunte cuántos elementos se agregarán a una lista. 
+# Se debe validar que el número sea mayor que cero, si no lo es, se debe volver a pedir hasta que el usuario teclee un número positivo.
+
+# Después debe leer esa cantidad de datos e ingresarlos a la lista.
+
+# El programa debe desplegar los elementos ingresados en forma inversa, el indice mayor al menor, como se muestra en el ejemplo.
+
+# Entradas
+# Número entero n que corresponde al número de elementos a ingresar a la lista. 
+# Debe ser mayor a 0 de lo contrario se solicitará de nuevo hasta que se cumpla la condición.
+# Posteriormente se reciben los n elementos de la lista (números enteros), uno en cada renglón.
+
