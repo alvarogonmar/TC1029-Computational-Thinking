221618bb050b5c6ce1e9343da0d93b4a1028b8a3
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 16:51:37 -0600

    4.py Finished

--- 2_PERIODO/practice_for_exam/4.py
+++ 2_PERIODO/practice_for_exam/4.py
@@ -14,3 +14,20 @@
 # La letra correspondiente al score asignado de acuerdo con la tabla de arriba.
 # Si la entrada no está dentro del rango de 0 a 1 (inclusive), la función debe regresar la leyenda "score incorrecto".
 
+def calculate_grade(number):
+    if number > 0.9:
+        print('A')
+    elif number > 0.8:
+        print('B')
+    elif number > 0.7:
+        print('C')
+    elif number > 0.6:
+        print('D')
+    elif number <= 0.6:
+        print('F')
+
+number = float(input('Introduce a number between 0-1: '))
+if number <= 1 and number >= 0:
+    calculate_grade(number=number)
+else:
+    print('Incorrect Score')
