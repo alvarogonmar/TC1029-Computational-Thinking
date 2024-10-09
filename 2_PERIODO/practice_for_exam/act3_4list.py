a9b9884b5f59859625f1309dbe8cc92087fcfb2c
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 17:48:30 -0600

    act3_4list.py added

---
+++ 2_PERIODO/practice_for_exam/act3_4list.py
@@ -0,0 +1,5 @@
+# Contar pares e impares en una lista
+# Escribe un programa que lea números en un rango de 0 y 2000 y los vaya agregando a una lista, 
+# dejará de leer cuando se introduzca un número negativo. Luego el programa debe contar la cantidad de valores pares e impares de la lista. Considera al 0 como par.
+# Entrada
+# Cero o más valores enteros. Finaliza la captura con un número negativo. Nota que el número negativo no se debe agregar a la lista.
