2ddcbdeef50c904d09160a7951f90c62b5b02901
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 17:42:25 -0600

    act3_3list.py finished

--- 2_PERIODO/practice_for_exam/act3_3list.py
+++ 2_PERIODO/practice_for_exam/act3_3list.py
@@ -11,3 +11,15 @@
 # Debe ser mayor a 0 de lo contrario se solicitará de nuevo hasta que se cumpla la condición.
 
 # Posteriormente se reciben los n elementos de la lista, uno en cada renglón.
+
+elements = int(input('Elements in the list: '))
+while elements < 0:
+    elements = int(input('Elements in the list: '))
+list1 = []
+for i in range(elements):
+    numbers = int(input(f'Introduce the number {i+1}: '))
+    list1.append(numbers)
+
+average = sum(list1) / len(list1)
+print(list1)
+print(average)
