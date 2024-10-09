638939a777cbeedb20087da02c2e9932506377fa
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-08 22:04:25 -0600

    Adding exercises for my exam

---
+++ 2_PERIODO/practice_for_exam/1.py
@@ -0,0 +1,14 @@
+#Realiza una función llamada funOperaciones que tenga un parámetro llamado a, un parámetro por default a 2 llamado b,
+#y un parámetro llamado op que indique mediante el operador aritmético recibido la operación aritmética a realizar. 
+#La función permite hacer una de las siguientes operaciones aritméticas: suma, resta, multiplicación, división, residuo o potencia, 
+# entre los parámetro a y b según el operador que reciba. 
+# Además la función no debe permitir realizar operación de residuo o división si b es cero y debe indicar: 
+# Error División Sobre Zero.
+#Por ejemplo, si se realiza el siguiente llamado a ejecución de la función esto es lo que imprimiría:
+#funOperaciones(12,3, "%")
+#La operación elegida es: Residuo
+#Al ejecutar la operación se obtiene: 12 % 3 = 0
+#Al final pega tu código aquí, no olvides que debe estar bien identado y en la sintaxis adecuada de python
+
+def  fun_operations():
+    
