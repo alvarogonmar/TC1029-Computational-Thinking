024aa80443c8d3edc6ab895f871175b63bd5988b
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 16:13:02 -0600

    3.py Created

---
+++ 2_PERIODO/practice_for_exam/3.py
@@ -0,0 +1,8 @@
+#Desarrolla un programa que contenga las siguientes funciones:
+#una función llamada area_cilindro, que reciba el radio y altura del cilindro y retorne el área calculada.
+#una función llamada volumen_cilindro, que reciba el radio y altura del cilindro y regrese el volumen calculado.
+#El programa debe leer el radio y la altura de un cilindro, luego llamar a las funciones y finalmente mostrar el área y volumen del cilindro.
+
+#Entrada
+#El radio del cilindro
+#La altura del cilindro
