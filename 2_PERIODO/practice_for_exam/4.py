f32f63706ebf3b207ca256b1a48317bc20de9713
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 16:32:52 -0600

    4.py added

---
+++ 2_PERIODO/practice_for_exam/4.py
@@ -0,0 +1,16 @@
+# Escribe un programa en el cual definas la función llamada calcula_grado. Esta función debe recibir un número flotante entre 0 y 1, 
+# y debe regresar una nota alfabética de acuerdo con la siguiente tabla.
+
+# Score	Nota
+# >0.9	A
+# >0.8	B
+# >0.7	C
+# >0.6	D
+# <=0.6	F
+ 
+# Entrada
+# Un número flotante entre 0 y 1.
+# Salida
+# La letra correspondiente al score asignado de acuerdo con la tabla de arriba.
+# Si la entrada no está dentro del rango de 0 a 1 (inclusive), la función debe regresar la leyenda "score incorrecto".
+
