ea04141ebb68ac16ba573bdb613adfb545940ca2
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 16:56:35 -0600

    Adding act3_1list.py

---
+++ 2_PERIODO/practice_for_exam/act3_1list.py
@@ -0,0 +1,13 @@
+# Escribe un programa que pregunte cuántos elementos se agregarán a una lista. 
+# Se debe validar que el número sea mayor que cero, si no lo es, se debe volver a pedir hasta que el usuario teclee un número positivo.
+
+# Después debe leer esa cantidad de datos e ingresarlos a la lista.
+
+# Finalmente mostrar la lista leída en el formato que se muestra en el ejemplo. 
+
+# Entradas
+
+# Número entero n que corresponde al número de elementos a ingresar a la lista. 
+# Debe ser mayor a 0 de lo contrario se solicitará de nuevo hasta que se cumpla la condición.
+# Posteriormente se reciben los n elementos de la lista, uno en cada renglón
+
