4b33b4496d71094b38652325cff8023c35a1c958
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 17:34:48 -0600

    act3_3list.py added

---
+++ 2_PERIODO/practice_for_exam/act3_3list.py
@@ -0,0 +1,13 @@
+#3
+# Escribe un programa que lea un valor entero n, y después n valores enteros y los agregue a una lista.
+
+# El programa debe calcular el promedio de los valores de la lista
+
+# Finalmente debe mostrar la lista y en el siguiente renglón el promedio de los valores que contiene.
+
+# Entradas
+
+# Número entero n que corresponde al número de elementos a ingresar a la lista. 
+# Debe ser mayor a 0 de lo contrario se solicitará de nuevo hasta que se cumpla la condición.
+
+# Posteriormente se reciben los n elementos de la lista, uno en cada renglón.
