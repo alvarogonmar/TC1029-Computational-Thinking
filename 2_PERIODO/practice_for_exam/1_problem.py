e5d5fce450bd80ad2228686f12e3f16fa5828161
alvarogonmar <alvarogonmar0@gmail.com>
2024-10-09 18:24:57 -0600

    Problem 1

---
+++ 2_PERIODO/practice_for_exam/1_problem.py
@@ -0,0 +1,11 @@
+# 1. Problema 1: Agenda de Contactos (Fácil-Medio)
+# Descripción: Crea un programa que permita almacenar y gestionar una agenda de contactos. 
+# El programa deberá permitir al usuario agregar, buscar y mostrar contactos, 
+# utilizando un diccionario donde las llaves sean los nombres y los valores sean los números de teléfono.
+
+# Requerimientos:
+
+# La función principal debe solicitar al usuario que elija una opción: agregar, buscar o mostrar contactos.
+# Al agregar un contacto, el usuario debe ingresar el nombre y el número de teléfono, que se guardarán en un diccionario.
+# Al buscar un contacto, el programa deberá solicitar el nombre y mostrar el número si está en la agenda.
+# Al mostrar, se deben listar todos los contactos almacenados.
